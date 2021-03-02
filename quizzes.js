38bc3a667f525b85dfe84a9ed1c1807c992b9357
OforOlive1234 <69664883+OforOlive1234@users.noreply.github.com>
2021-03-02 11:59:59 +0800

    Merge branch 'master' of https://github.com/personalityquiz/personalityquiz.github.io

--- quizzes.js
+++ quizzes.js
@@ -328,6 +328,89 @@ function getQuiz(quizID) {
       },
     };
   }
+  if (quizID == "psychopath-test") {
+    return {
+      questions: [
+        {
+          question: "Are you gay?",
+          responses: ["Very", "Not much, but sometimes","BLODDY HEKC NUO"],
+          functions: [
+            () => {
+              quizVars.gay = true;
+            },
+            () => {
+              quizVars.points++;
+            },
+            () => {},
+          ],
+        },
+        {
+          question: "Do you consider yourself a smooth liar",
+          responses: ["True", "False"],
+          functions: [
+            () => {
+              quizVars.points++;
+            },
+            () => {},
+          ],
+        },
+        {
+          question: "Why are we still here?",
+          responses: ["Just to suffer", "This is fun", "No, I don't think I will"],
+          functions: [
+            () => {
+              quizVars.points += 2;
+            },
+            () => {
+              quizVars.points++;
+            },
+            () => {},
+          ],
+        },
+        {
+          question: "Do you like to kill the peoples",
+          responses: ["True", "False"],
+          functions: [
+            () => {
+              quizVars.psycho = true;
+            },
+            () => {},
+          ],
+        },
+        {
+          question: "Can you use empathy in high stress situations",
+          responses: ["Probably", "That's not how it works"],
+          functions: [
+            () => {
+              quizVars.points++;
+            },
+            () => {},
+          ],
+        },
+      ],
+      endFunction: () => {
+        if (quizVars.psycho) {
+          return "YOU ARE THE MURERDERRER YOU ARE MOST SCARY PSYCHOMAN ALIVE!!";
+        } else if (quizVars.gay) {
+          return "Your score is 5/5. You are one of the coolest person around.";
+        } else if (quizVars.points == 4) {
+          return "Your score is 4/5. You are cool, but not the coolest";
+        } else if (quizVars.points == 3) {
+          return "Your score is 3/5. You are average coolness.";
+        } else if (quizVars.points == 2) {
+          return "Your score is 2/5. You are not very cool. Consider taking coolness class.";
+        } else if (quizVars.points == 1) {
+          return "Your score is 1/5. You are uncool full stop. Get cooler, please.";
+        } else if (quizVars.points == 0) {
+          return "Your score is 0/5. You are extremely uncool, get help from psychiatrist.";
+        }
+      },
+      startFunction: () => {
+        quizVars.points = 0;
+        quizVars.jazz = false;
+      },
+    };
+  }
   if (quizID == "compuper") {
     return {
       questions: [
